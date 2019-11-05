43d18f0a0ff4ce65bf6acecc145a4318d511fb4a
sihan9 <1060192071@qq.com>
2019-11-05 17:44:40 +0800

    add cnode

---
+++ cnode/precache-manifest.beb5b35a4f6add8c9efd4fcba2ed3bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d251cd4f844336190e01a2137a8057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dc9f845fc99cc5f6579",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "d31d9efa8bc74b0eb4d7",
+    "url": "/static/css/main.78acae9e.chunk.css"
+  },
+  {
+    "revision": "0dc9f845fc99cc5f6579",
+    "url": "/static/js/2.5ac05eba.chunk.js"
+  },
+  {
+    "revision": "d31d9efa8bc74b0eb4d7",
+    "url": "/static/js/main.af43f5ae.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
